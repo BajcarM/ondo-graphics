f87f92a1719680234fb97650897d84a905e34a1c
BajcarM <102542768+BajcarM@users.noreply.github.com>
2023-12-12 21:06:04 +0100

    storybook fix

--- packages/waves/src/Waves.stories.ts
+++ packages/waves/src/Waves.stories.ts
@@ -55,7 +55,7 @@ export const Template: StoryObj = {
       style="height: 1000px; width: 1000px; display: block; border: 1px solid black;
 
       margin-inline: auto;"
-      background=${JSON.stringify({
+      background="${JSON.stringify({
         linearGradient: {
           direction: 'toRight',
           colors: [
@@ -73,7 +73,7 @@ export const Template: StoryObj = {
             },
           ],
         },
-      })}
-      waves=${JSON.stringify(WAVE_OPTIONS)}
+      })}"
+      waves="${JSON.stringify(WAVE_OPTIONS)}"
     ></og-waves>`,
 }
